638e481a0d307438a1a38202b3554d826e9ef3e7
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-17 21:22:55 -0600

    manager.js code added

--- Develop/lib/manager.js
+++ Develop/lib/manager.js
@@ -1,1 +1,10 @@
-//
+const Employee = require("./employee")
+class Manager extends Employee {
+    constructor (name, id, email, officeNumber) {
+        super(name, id, email)
+        this.officeNumber = officeNumber
+    }
+    getOfficeNumber() {return this.officeNumber}
+    GetRole() {return "Manager"}
+}
+module.exports = Manager
