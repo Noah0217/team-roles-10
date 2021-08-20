5b30fa383770ef89ec28a6b8500afee2ad9996af
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-19 21:22:41 -0600

    started app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-//
+const Employee = require("./Develop/lib/employee");
+const Engineer = require("./Develop/lib/engineer");
+const Intern = require("./Develop/lib/intern");
+const Manager = require("./Develop/lib/manager");
+
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const jest = require("jest");
+let Team = [];
+
