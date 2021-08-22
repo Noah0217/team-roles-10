c14a24f74dd5b70bbef1720f9315a46a1002d3c5
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-22 17:37:51 -0600

    added comments

--- app.js
+++ app.js
@@ -8,13 +8,12 @@ const Manager = require("./Develop/lib/manager");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const jest = require("jest");
 const Team = [];
 
 //File Path 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-const render = require("./Develop/lib/render");
+const render = require("./Develop/lib/htmlRenderer");
 var managerCounter = 0;
 
 const teamMembers = {
